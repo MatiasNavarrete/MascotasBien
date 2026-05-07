3261824c4dd14b6901ffd8f6f9f23557a33e21f7
Your Name <mat.navarreted@duocuc.cl>
2026-05-07 12:29:03 -0400

    feat:proyecto principal de mascotasbien

---
+++ MascotasBien/src/main/java/com/example/MascotasBien/MascotasBienApplication.java
@@ -0,0 +1,13 @@
+package com.example.MascotasBien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotasBienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotasBienApplication.class, args);
+	}
+
+}
---
+++ MascotasBien/src/test/java/com/example/MascotasBien/MascotasBienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MascotasBien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotasBienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
