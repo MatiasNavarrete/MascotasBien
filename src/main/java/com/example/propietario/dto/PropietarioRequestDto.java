daf9db87977849b49bffc5fbb920ac5736344d33
Your Name <mat.navarreted@duocuc.cl>
2026-05-08 22:48:55 -0400

    feat: se agregaron mensajes de error para campos invalidos de numero email y tipo de propietarios

--- src/main/java/com/example/propietario/dto/PropietarioRequestDto.java
+++ src/main/java/com/example/propietario/dto/PropietarioRequestDto.java
@@ -5,19 +5,23 @@ import com.example.propietario.enums.EstadoCuenta;
 import com.example.propietario.enums.TipoPropietario;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record PropietarioRequestDto(
 
         @NotBlank(message = "El nombre es obligatorio")
         String name,
         @NotBlank(message = "El email es obligatorio")
-        @Email
+        @Email(message = "Debes ingresar un metodo de email valido")
         String email,
         @NotBlank(message = "El numero de telefono es obligatorio")
+        @Pattern(regexp = "^\\+569][0-9]{8}$", message = "El formato debe ser +569XXXXXXXX")
         String phoneNumber,
         String address,
         String secondaryContactName,
         String secondaryContactPhone,
+        @NotNull(message = "El tipo de propietario es obligatorio")
         TipoPropietario tipoPropietario,
         EstadoCuenta estadoCuenta,
         EstadoBusqueda estadoBusqueda
--- src/main/java/com/example/propietario/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/propietario/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.example.propietario.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +19,18 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse(exception.getMessage(), HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 
     }
 
