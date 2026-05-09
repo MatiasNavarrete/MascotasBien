99ad96487f794acdcc8fd0a7bdc3b11f3a5d66f8
josem <jm.romero@duocuc.cl>
2026-05-09 19:07:57 -0400

    feat: Implementa BaseEntity

--- src/main/java/com/example/mascota/entity/BaseEntity.java
+++ src/main/java/com/example/mascota/entity/BaseEntity.java
@@ -1,4 +1,33 @@
 package com.example.mascota.entity;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_active")
+    private boolean isActive = true;
+
 }
