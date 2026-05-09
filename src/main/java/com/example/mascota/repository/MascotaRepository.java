4622d363c29773810ec414e4c5e55e2d7589b4e0
josem <jm.romero@duocuc.cl>
2026-05-09 19:37:52 -0400

    feat: Implementa MascotaRepository.

--- src/main/java/com/example/mascota/repository/MascotaRepository.java
+++ src/main/java/com/example/mascota/repository/MascotaRepository.java
@@ -1,4 +1,29 @@
 package com.example.mascota.repository;
 
-public class MascotaRepository {
+import com.example.mascota.entity.MascotaEntity;
+import com.example.mascota.enums.PetStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<MascotaEntity, UUID> {
+
+    //Buscar todas las mascotas de un dueño según la id del dueño
+    List<MascotaEntity> findByOwnerId(UUID ownerId);
+
+    //Buscar por estado (Para ver quiénes están perdidos, avistados, etc.)
+    List<MascotaEntity> findByStatus(PetStatus status);
+
+    //Buscar por especie (Para filtrar si buscas un perro o un gato por ejemplo)
+    List<MascotaEntity> findBySpeciesIgnoreCase(String species);
+
+    //Buscar por raza
+    List<MascotaEntity> findByBreedContainingIgnoreCase(String breed);
+
+    //Combinado: Buscar mascotas perdidas por especie
+    List<MascotaEntity> findByStatusAndSpecies(PetStatus status, String species);
+
 }
