568a546837def1bf3e7e9cc3e5c7f2211fca237a
josem <jm.romero@duocuc.cl>
2026-05-09 19:10:38 -0400

    feat: Implementa PetStatus en paquete enums.

---
+++ src/main/java/com/example/mascota/enums/PetStatus.java
@@ -0,0 +1,9 @@
+package com.example.mascota.enums;
+
+public enum PetStatus {
+
+        LOST, //PERDIDA
+        FOUND, //ENCONTRADA (POR OTRA PERSONA)
+        SIGHTED, //AVISTADA (VISTA EN UN LUGAR, PERO NO RETENIDA)
+        RECOVERED
+}
