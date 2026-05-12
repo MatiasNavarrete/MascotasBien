a80835c6e6b44e56966c64c5d820e0302cef8ea5
josem <jm.romero@duocuc.cl>
2026-05-11 21:33:30 -0400

    feat: Implementa implementa MascotaService en service.

--- src/main/java/com/example/mascota/service/MascotaService.java
+++ src/main/java/com/example/mascota/service/MascotaService.java
@@ -1,4 +1,13 @@
 package com.example.mascota.service;
 
-public class MascotaService {
+import com.example.mascota.dto.MascotaRequestDTO;
+import com.example.mascota.entity.MascotaEntity;
+import java.util.List;
+import java.util.UUID;
+
+public interface MascotaService {
+    MascotaEntity registrarMascota(MascotaRequestDTO dto);
+    List<MascotaEntity> listarPorPropietario(UUID ownerId);
+    List<MascotaEntity> listarTodas();
+
 }
