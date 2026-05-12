f250e00f8bc5bfc0cf8cc39f8d3b671e948d4033
Your Name <mat.navarreted@duocuc.cl>
2026-05-12 16:00:11 -0400

    feat: inicializacion del microservicio bff

---
+++ src/main/java/com/example/ms_pet_bff/MsPetBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_pet_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPetBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPetBffApplication.class, args);
+	}
+
+}
