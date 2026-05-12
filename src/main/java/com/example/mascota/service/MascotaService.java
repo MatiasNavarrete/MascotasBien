e356f025e120ecfa0d0048d5424062b5bb879164
josem <jm.romero@duocuc.cl>
2026-05-11 22:59:29 -0400

    refactor: ahora MascotaService utiliza DTOs.

--- src/main/java/com/example/mascota/service/MascotaService.java
+++ src/main/java/com/example/mascota/service/MascotaService.java
@@ -1,13 +1,19 @@
 package com.example.mascota.service;
 
 import com.example.mascota.dto.MascotaRequestDTO;
-import com.example.mascota.entity.MascotaEntity;
+import com.example.mascota.dto.MascotaResponseDTO;
+import com.example.mascota.dto.MascotaUpdateDTO;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface MascotaService {
-    MascotaEntity registrarMascota(MascotaRequestDTO dto);
-    List<MascotaEntity> listarPorPropietario(UUID ownerId);
-    List<MascotaEntity> listarTodas();
 
+    MascotaResponseDTO registrarMascota(MascotaRequestDTO dto);
+
+    List<MascotaResponseDTO> listarPorPropietario(UUID ownerId);
+
+    List<MascotaResponseDTO> listarTodas();
+
+    MascotaResponseDTO update(UUID id, MascotaUpdateDTO dto);
 }
