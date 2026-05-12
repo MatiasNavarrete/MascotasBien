be43ff596f2688b3e52cbb0841d8d9b202fd9977
josem <jm.romero@duocuc.cl>
2026-05-11 22:17:14 -0400

    refactor: campo corregido nuevamente. daba error al compilar

--- src/main/java/com/example/mascota/mapper/MascotaMapper.java
+++ src/main/java/com/example/mascota/mapper/MascotaMapper.java
@@ -12,7 +12,7 @@ public interface MascotaMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "isActive", ignore = true)
+    @Mapping(target = "active", ignore = true)
     MascotaEntity toEntity(MascotaRequestDTO dto);
 
     MascotaResponseDTO toResponseDto(MascotaEntity entity);
