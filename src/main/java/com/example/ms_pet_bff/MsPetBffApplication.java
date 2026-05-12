3ce7fb8cce650c2a85ecb0bc4ef02becd28406e5
josem <jm.romero@duocuc.cl>
2026-05-12 17:00:02 -0400

    feat: añade @EnableFeignClients en MsPetBffApplication

--- src/main/java/com/example/ms_pet_bff/MsPetBffApplication.java
+++ src/main/java/com/example/ms_pet_bff/MsPetBffApplication.java
@@ -2,8 +2,10 @@ package com.example.ms_pet_bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsPetBffApplication {
 
 	public static void main(String[] args) {
