415b2a3a645fdb846ff3fefc3cd700c9b96a9cae
josem <jm.romero@duocuc.cl>
2026-05-12 23:03:09 -0400

    fix: corrige datos para coincidir con microservicio mascotas.

--- src/main/java/com/example/ms_pet_bff/dto/MascotaResponseDto.java
+++ src/main/java/com/example/ms_pet_bff/dto/MascotaResponseDto.java
@@ -1,11 +1,9 @@
 package com.example.ms_pet_bff.dto;
 
-import com.example.ms_pet_bff.enums.EstadoBusqueda;
-
 public record MascotaResponseDto(
-        String nameMascota,
-        String raza,
-        String especie,
-        EstadoBusqueda estadoBusqueda
+        String name,
+        String breed,
+        String species,
+        String status
 ) {
 }
