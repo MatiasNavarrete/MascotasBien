2a7eb6b0d9296a5f3a636908ee1a6d5bec9c2cc0
josem <jm.romero@duocuc.cl>
2026-05-12 23:04:46 -0400

    fix: corrige nombres en rutas.

--- src/main/java/com/example/ms_pet_bff/client/MascotaClient.java
+++ src/main/java/com/example/ms_pet_bff/client/MascotaClient.java
@@ -4,12 +4,18 @@ import com.example.ms_pet_bff.dto.MascotaResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @FeignClient(name = "mascota", url = "http://localhost:8081/api/v1/mascotas")
 public interface MascotaClient {
     @GetMapping("/propietario/{ownerId}")
     List<MascotaResponseDto> getByPropietarioId(@PathVariable("ownerId")UUID ownerId);
+
+    @PostMapping
+    Object crearMascota(@RequestBody Map<String, Object> mascotaData);
 }
